4ad066766c081a5fa9326d3b90c0c8981c5ac3fc
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-01 20:06:04 -0300

    chore: add icon button

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
+
+import { StyleSheet } from 'react-native';
+import { colors } from '../../consts';
+
+type IconButtonProps = {
+  onPress: () => void;
+  icon: JSX.Element;
+};
+const IconButton = ({ onPress, icon }: IconButtonProps) => (
+  <View>
+    <TouchableOpacity
+      style={[styles.singleButton]}
+      onPress={onPress}
+      activeOpacity={0.85}
+    >
+      {icon}
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  singleButton: {
+    backgroundColor: colors.white,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 0.3,
+    elevation: 1,
+    padding: 15,
+  },
+});
+
+export default IconButton;
