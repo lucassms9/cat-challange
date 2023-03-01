bdf5fe81e9fa72c2b2ee4ca1231cadc2a34e0c74
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-01 20:06:16 -0300

    chore: add consts

---
+++ src/consts/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  background: '#fbfaff',
+  white: '#fff',
+  black: '#434141',
+  gray: '#BFBFC0',
+  nope: '#E5566D',
+  like: '#4CCC93',
+  favorite: '#3CA3FF',
+};
+
+export default colors;
---
+++ src/consts/index.ts
@@ -0,0 +1,6 @@
+import colors from './colors';
+
+const token =
+  'live_EDUoGhl2wvGppQMe6TLMvhSjusY4awHCH6Cc2Hs9rbwG2rOMMrKGp4tSaFNKKwpE';
+
+export { colors, token };
